2bd7de30a6b7417d9eb30b4b855172f3aa4a4067
PreskoIsTheGreatest <preskosemov@abv.bg>
2016-04-30 23:57:39 +0300

    next fix

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ app.engine('html', require('ejs').renderFile);
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({extended: true}));
 app.use(require('express-session')({secret: 'prostotiq', resave: false, saveUninitialized: false}));
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
